483a07d4ef603b35e0d25c566fcab92300e040e8
Your Name <you@example.com>
2025-03-21 14:11:39 +0700

    SE02: improve

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
+        Student student1 = new Student("John Doe", "S12345");
+        student1.setCredits(15);
+        student1.setTotal(45);
         
+        System.out.println("Student Name: " + student1.getName());
+        System.out.println("Student ID: " + student1.getID());
+        System.out.println("Credits: " + student1.getCredits());
+        System.out.println("Total Points: " + student1.getTotal());
+        System.out.println("GPA: " + student1.gpa());
     }
 }
