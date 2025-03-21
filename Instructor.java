a77e21a60d21e8fa1d846fc6fc03c69bb5efdd6f
LVQuang <ql7769663@gmail.com>
2025-03-21 13:17:52 +0700

    SE02: Compelete Instructor

--- Instructor.java
+++ Instructor.java
@@ -2,4 +2,25 @@ public class Instructor {
     private String name;
     private String id;
     private String department;
+
+    public Instructor (String name, String id){
+        this.name=name;
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public String getID(){
+        return id;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
